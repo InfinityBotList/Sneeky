e7ae987351d5171712ded7b9ecc1fac81bcb6341
TheRealToxicDev <toxic.dev09@gmail.com>
2023-04-03 01:19:54 -0600

    feat(add): Ready event

---
+++ src/typings/intents.ts
@@ -0,0 +1,26 @@
+import { Intents } from 'discord.js';
+
+const IClientInteractions = {
+    AutoMod: {
+        Config: Intents.FLAGS.AUTO_MODERATION_CONFIGURATION,
+        Execute: Intents.FLAGS.AUTO_MODERATION_EXECUTION 
+    },
+    Guilds: {
+        BaseIntent: Intents.FLAGS.GUILDS,
+        GuildBans: Intents.FLAGS.GUILD_BANS,
+        GuildEmoji: Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+        GuildInteg: Intents.FLAGS.GUILD_INTEGRATIONS,
+        GuildInvite: Intents.FLAGS.GUILD_INVITES,
+        GuildMembers: Intents.FLAGS.GUILD_MEMBERS,
+        GuildMessages: Intents.FLAGS.GUILD_MESSAGES,
+        GuildMsgReact: Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        GuildMsgTyping: Intents.FLAGS.GUILD_MESSAGE_TYPING,
+        GuildPresences: Intents.FLAGS.GUILD_PRESENCES,
+        GuildWebhooks: Intents.FLAGS.GUILD_WEBHOOKS
+    },
+    Messages: {
+        MsgContent: Intents.FLAGS.MESSAGE_CONTENT
+    }
+}
+
+export default IClientInteractions;
---
+++ src/typings/types.ts
@@ -0,0 +1,79 @@
+import type { CommandInteraction, GuildMember, Collection, HexColorString, Guild } from 'discord.js';
+
+/** BASE INTERFACES START HERE */
+export interface IGuildDB {
+    _id: string;
+    guildID: string;
+    guildName: string;
+    prefix: string;
+    status: boolean;
+    invitations: boolean;
+    logChannel: string;
+    premium: boolean;
+    commands: Array<any>;
+}
+
+export interface IEmojis {
+    [index: string]: string;
+}
+
+export interface IColors {
+    [index: string]: HexColorString;
+}
+
+export interface IConfig {
+    [index: string]: any
+}
+
+export interface IWebhookSend {
+    avatar?: string;
+    username?: string;
+    embeds?: Array<any>;
+    content?: string;
+    code?: string;
+}
+
+/** GUILD INTERFACES START HERE */
+export interface IGuildMinDB {
+    id: string;
+    name: string;
+}
+
+/** OBJECT INTERFACES START HERE */
+export interface IObject {
+    [index: string]: any;
+}
+
+/** COMMAND INTERFACES START HERE */
+export interface ICommandOptions {
+    name: string;
+    aliases: string[];
+    args?: Array<ICommandInfosArgs>;
+    options?: Array<ICommandInfosArgs>;
+    category: string;
+    description: string;
+    cooldown: number;
+    userPermissions: string[];
+    botPermissions: string[];
+    subCommands: any[];
+}
+
+export interface ICommandInfosArgs {
+	name: string;
+	description: string;
+	type: string;
+	required: boolean;
+}
+
+export interface ICommandInteraction extends CommandInteraction {
+	replySuccessMessage(content: string): Promise<void>
+	replyErrorMessage(content: string): Promise<void>
+	subcommand: string | null
+	member: GuildMember
+}
+
+export interface IGuildEvents extends Guild {
+    guild: Guild;
+}
+
+export interface ICommandArgs extends Collection<string, any> { }
