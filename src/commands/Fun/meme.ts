debf27870f949a1b6699a6e2254f8cb1ddd678f6
TheRealToxicDev <TheRealToxicDev@users.noreply.github.com>
2023-04-13 08:35:19 +0000

    🥰 chore(ci): Make the code pretty

--- src/commands/Fun/meme.ts
+++ src/commands/Fun/meme.ts
@@ -144,7 +144,6 @@ export default class extends Command {
 
         collector.on('collect', async (i: any) => {
             if (i.customId === 'generate-meme') {
-
                 const meme = await fetch(`https://reddit.com/r/${category}${sort}&t=week${limit}`).then(r => r.json())
 
                 let nsfwCheck
@@ -214,7 +213,6 @@ export default class extends Command {
                     ]
                 })
             } else if (i.customId === 'close-memes') {
-
                 await i.editReply({
                     embeds: [
                         new MessageEmbed()
